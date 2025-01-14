4160fc59f66460a65441ff3d0e04bbb17539e78a
CLemmons77 <caleblego7@gmail.com>
2025-01-14 15:20:52 -0500

    padding fix in work projects section for mobile devices

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -26,7 +26,7 @@ const Projects = () => {
     <section className="c-space my-20" id="work">
       <p className="head-text">My Work</p>
       <div className="grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full">
-        <div className="flex flex-col gap-5 relative sm:p-10 px-5 shadow-2xl shadow-black-200">
+        <div className="flex flex-col gap-5 relative sm:p-10 py-5 px-5 shadow-2xl shadow-black-200">
           <div className="absolute top-0 right-0">
             <img
               src={currentProject.spotlight}
